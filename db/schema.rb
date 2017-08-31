d70cee86e0203d3f0f0cc7ee8c97798f6515fdbc
Nick Paridon <paridonn1@gmail.com>
2017-08-31 12:19:21 -0400

    testing full Piano JS

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170710182757) do
+ActiveRecord::Schema.define(version: 20170809195548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
