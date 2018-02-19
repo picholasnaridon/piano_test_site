bbe0e6281fd89d85188693835d90cd12009246f0
Nick Paridon <paridonn1@gmail.com>
2018-02-19 13:51:19 -0500

    flyout example

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,2 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(function () {
+    $("#slideout").click(function () {
+        if($(this).hasClass("popped")){
+        $(this).animate({right:'-18.5%'}, {queue: false, duration: 500}).removeClass("popped");
+        console.log("right -280")
+    }else {
+        $(this).animate({right: "0%" }, {queue: false, duration: 500}).addClass("popped");
+        console.log("right 0")
+      }
+    });
+});
